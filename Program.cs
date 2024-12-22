076770348a2b1d16bae0136d01c3ee2e6842dbfb
Beratdmrbs <beratdmrbs61@gmail.com>
2024-12-22 16:54:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Bir sayı girin: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int a = 0, b = 1;
+
+        Console.Write("Fibonacci Serisi: ");
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i == 0)
+            {
+                Console.Write(a);
+            }
+            else if (i == 1)
+            {
+                Console.Write(" " + b);
+            }
+            else
+            {
+                int next = a + b;
+                Console.Write(" " + next);
+                a = b;
+                b = next;
+            }
+        }
+
+        Console.WriteLine();
+    }
+}
